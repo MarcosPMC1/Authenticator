923f90423fef91e89ffbee341305d4378f66e391
Marcos Cardoso <marcospmc@outlook.com>
2025-03-11 19:38:20 -0300

    feat(docs): implement swagger in /users

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,18 +5,28 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginAuthDto } from './dto/login-auth.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiResponse, ApiTags, getSchemaPath } from '@nestjs/swagger';
+import { LoginResponse } from './dto/login-response.dto';
+import { RegistrateResponse } from './dto/registrate-response.dto';
 
 @ApiTags('Authenticate')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiCreatedResponse({
+    description: 'Success registrate user',  
+    type: RegistrateResponse
+  })
   @Post('registrate')
   registrateAuth(@Body() data: LoginAuthDto) {
     return this.authService.registrate(data);
   }
 
+  @ApiCreatedResponse({
+    description: 'Sucess login',
+    type: LoginResponse
+  })
   @Post()
   loginAuth(@Body() data: LoginAuthDto) {
     return this.authService.login(data);
--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,10 +1,17 @@
 import { IsEmail, IsEnum, IsUUID } from "class-validator";
 import { Role } from "../../enums/role.enum";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class UserDto{
+    @ApiProperty({
+        example: 'email@email.com'
+    })
     @IsEmail()
     email: string
 
+    @ApiProperty({
+        example: 'user'
+    })
     @IsEnum(Role)
     role: Role
 }
